dc33b5e113eafc4b2538bf1ccd745d55dc288197
jojostx <ikuskid7@gmail.com>
2023-04-01 23:07:08 -0700

    solutions

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-
 curl -siL -X OPTIONS "$1" | grep -iF "Allow:" | cut -d " " -f 2-
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that sends a POST request to a URL, and displays the body of the response
-
 curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
